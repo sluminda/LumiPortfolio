ab94b6ba701ee2dd6f3be89ba083d0e423a59558
Luminda Saranga <sluminda@gmail.com>
2024-01-21 06:31:24 +0530

    Initial Commit

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#sTwo", {
+  strings: [
+    "Front-End Web Developer",
+    "Hardware Technician",
+    "Network Technician",
+  ],
+  typeSpeed: 70,
+  backSpeed: 50,
+  Delay: 20,
+  loop: true,
+});
