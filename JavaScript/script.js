e8e30f691a97ed10542d27e1e1c3a75d0afd2a69
Luminda Saranga <sluminda@gmail.com>
2024-01-22 04:28:14 +0530

    Final

---
+++ JavaScript/script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+var typed = new Typed("#occupation", {
+  strings: ["Front-End Developer", "Hardware Technician", "Network Technician"],
+  typeSpeed: 70,
+  backSpeed: 50,
+  Delay: 20,
+  loop: true,
+});
