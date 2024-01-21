f3ea6f8b635f494a09fdd88e7cc67d378f122793
Luminda Saranga <sluminda@gmail.com>
2024-01-21 15:36:09 +0530

    Second Commit

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,24 @@
-var typed = new Typed("#sTwo", {
+// Header Navigation
+
+const navShowBtn = document.querySelector("#hamburg");
+const navCloseBtn = document.querySelector("#close");
+const navigation = document.querySelector("header nav ul");
+
+navShowBtn.addEventListener("click", (e) => {
+  navShowBtn.classList.add("hidden");
+  navCloseBtn.classList.remove("hidden");
+  navigation.classList.add("nav-show");
+  navigation.classList.remove("navigation");
+});
+
+navCloseBtn.addEventListener("click", (e) => {
+  navCloseBtn.classList.add("hidden");
+  navShowBtn.classList.remove("hidden");
+  navigation.classList.remove("nav-show");
+  navigation.classList.add("navigation");
+});
+
+var typed = new Typed("#occupation", {
   strings: [
     "Front-End Web Developer",
     "Hardware Technician",
